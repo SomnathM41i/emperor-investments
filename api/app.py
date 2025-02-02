2df0b61b0f88dc56451e06a186294c18e925c6a4
SomnathM41i <somnath.malim46@gmail.com>
2025-02-02 00:56:18 -0500

    Update home route to return a simple greeting instead of rendering index.html

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # Serve index.html as the first file
+    return "hello" # Serve index.html as the first file
 
 @app.route("/login", methods=["GET"])
 def login_page():
